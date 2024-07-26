40f3b8f4b4fba71144621166954c128e1bbf53af
nathan-cook <nathan@pragma.com.au>
2024-07-26 23:02:12 +1000

    moving files into root

--- astro/postcss.config.cjs
+++
@@ -1,7 +0,0 @@
-const autoprefixer = require("autoprefixer")
-const postcssImport = require("postcss-import")
-const postcssNested = require("postcss-nested")
-const postcssCustomMedia = require("postcss-custom-media")
-module.exports = {
-  plugins: [postcssImport(), postcssNested(), postcssCustomMedia(), autoprefixer()],
-}
